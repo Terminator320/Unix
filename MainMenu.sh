2c164945e5939723eb2cc2bab729e913d09e0816
Terminator320 <george@vogas.net>
2025-12-04 01:49:15 -0500

    added the colors for the last 3 option (Service,User,File)

--- MainMenu.sh
+++ MainMenu.sh
@@ -1,15 +1,16 @@
+
 #main menu
 MainMenu(){
 while true; do
   echo -e "\e[37m=======================================  Main Menu  =======================================\e[0m"
   echo " "
-  echo -e "\e[1;36m 1) System Status\e[0m"
-  echo -e "\e[1;35m 2) Backup Management\e[0m"
-  echo -e "\e[1;33m 3) Network Management\e[0m"
-  echo -e "\e[1;34m 4) Service Management\e[0m"
-  echo -e "\e[1;30m 5) User Management\e[0m"
-  echo -e "\e[1;37m 6) File Management\e[0m"
-  echo -e "\e[1;31m 7) Exit Program\e[0m"
+  echo -e "\e[1;36m1) System Status\e[0m"
+  echo -e "\e[1;35m2) Backup Management\e[0m"
+  echo -e "\e[1;33m3) Network Management\e[0m"
+  echo -e "\e[1;34m4) Service Management\e[0m"
+  echo -e "\e[38;5;51m5) User Management\e[0m"
+  echo -e "\e[38;5;208m6) File Management\e[0m"
+  echo -e "\e[1;31m7) Exit Program\e[0m"
 
   read -p "Select an option [1-7]: " opt
 
@@ -304,7 +305,7 @@ confirmation(){
      echo -e "\e[1;32mYou have started $1\e[0m"
   #if its not active then user hasn't stop it
   else
-    echo -e "\e[1;31mYou have stopped $1\e[0m"
+    echo -e "\e[1;32mYou have stopped $1\e[0m"
   fi
 }
 
@@ -444,9 +445,9 @@ echo -e "\e[1;91m============================  Grant Root Premission  ==========
 	if [ "$ans" == "y" ]; then
     	   #adding root perms to the user
     	   sudo usermod -aG sudo "$user"
-	   echo -e "\e[1;32mThe user $user now has sudo permission.\e[0m"
+	   echo -e "\e[1;32mThe user '$user' now has sudo permission.\e[0m"
 	 elif [ "$ans" == "n" ]; then
-	   echo -e "\e[1;32m'$user' will not be given sudo permission.\e[0m"
+	   echo -e "\e[1;32mThe user '$user' will not be given sudo permission.\e[0m"
 	else
 	  echo -e "\e[1;31mInvalid option.\e[0m"
 	fi
@@ -477,16 +478,16 @@ deleteUser() {
 	#id yes then delete the user
 	if [ "$answer" == "y" ]; then
 		sudo userdel "$user"
-       		echo -e "/e[1;32m$user has been deleted succesfully.\e[0m"
+       		echo -e "\e[1;32mThe user '$user' has been deleted succesfully.\e[0m"
 
 	#if no do not delete the user
     	elif [ "$answer" == "n" ]; then
-        	echo -e "\e[1;32m'$user' will not be deleted.\e[0m"
+        	echo -e "\e[1;32mThe user '$user' will not be deleted.\e[0m"
     	else
         	echo -e "\e[1;31mInvalid input.\e[0m"
     	fi
   else
-    echo -e "\e[1;31m'$user' doesn't exists.\e[0m"
+    echo -e "\e[1;31mThe user '$user' doesn't exists.\e[0m"
   fi
 }
 
@@ -500,7 +501,7 @@ displayUsers(){
 #disconnect a remote user
 killRemote(){
   echo " "
-  echo "\e[1;96m==============================  Disconnecting Remote Users  ==============================\e[0m"
+  echo -e "\e[1;96m==============================  Disconnecting Remote Users  ==============================\e[0m"
   echo "All remote user: "
   who | grep pts | awk '{print $1,$2}'
 
@@ -517,10 +518,10 @@ killRemote(){
 		   #double checking if they want to disconncet the user
     		   read -p "Are you sure you want to disconnect $user: (y/n) ? " ans
                 	if [ "$ans" == "y" ]; then
-            		    echo -e "\e[1;32m'$user' disconnected.\e[0m"
+            		    echo -e "\e[1;32mThe user '$user' disconnected.\e[0m"
             		    sudo pkill -HUP -t "$pts"
                 	elif [ "$ans" == "n" ]; then
-                    	     echo -e "\e[1;32m'$user' will not be disconnected.\e[0m"
+                    	     echo -e "\e[1;32mThe user '$user' will not be disconnected.\e[0m"
                 	else
                     	     echo -e "\e[1;32mInvalid option\e[0m."
                 	fi
@@ -535,8 +536,8 @@ killRemote(){
 
 #show all groups for a user
 groupsUser(){
-echo " "
-echo "=======================  Groups Users  ================="
+  echo " "
+  echo -e "\e[1;93m=======================  Groups Users  ===================\e[0m"
 #showing the user all the users so they can pick
   echo "Here are a list of all users: "
   formatOut=$(cut -d: -f1 /etc/passwd | pr -t -a -4)
@@ -551,7 +552,7 @@ echo "=======================  Groups Users  ================="
     #getting all group then only displaying the name of the group
     groups "$user" | cut -f2 -d:
   else
-    echo "User does not exists."
+    echo -e "\e[1;31mUser does not exists.\e[0m"
   fi
 }
 
@@ -559,7 +560,7 @@ echo "=======================  Groups Users  ================="
 #method to add a user to a group
 addGroup(){
   echo " "
-  echo "====================================  Adding User to a group  ===================================="
+  echo -e "\e[1;38;5;214m====================================  Adding User to a group  ====================================\e[0m"
 
   #show all users
   echo "Here are a list of all users: "
@@ -587,17 +588,17 @@ addGroup(){
            #if yes add the user to the group if not do not
 	   if [ "$ans" == "y" ]; then
                 sudo usermod -aG "$group" "$user"
-                echo "$user has been added to $group group."
+                echo -e "\e[1;32mThe user '$user' has been added to $group group.\e[0m"
            elif [ "$ans" == "n" ]; then
-                 echo "$user will not be added to the group."
+                 echo -e "\e[1;32mThe user '$user' will not be added to the group.\e[0m"
            else
-                echo "Invalid option."
+                echo -e "\e[1;31mInvalid option.\e[0m"
            fi
       else
-        echo "$group does not exist."
+        echo -e "\e[1;31m$group does not exist.\e[0m"
       fi
   else
-        echo "$user doesn't exist."
+        echo -e "\e[1;31m$user doesn't exist.\e[0m"
   fi
 }
 
@@ -605,7 +606,7 @@ addGroup(){
 #remove a user from a group
 removeGroup(){
   echo " "
-  echo "====================================  Removing User from a group  ===================================="
+  echo -e "\e[1;38;5;212m====================================  Removing User from a group  ====================================\e[0m"
   #show all users
   echo "Here are a list of all users: "
   formatOut=$(cut -d: -f1 /etc/passwd | pr -t -a -4)
@@ -632,18 +633,18 @@ removeGroup(){
         read -p "Are you sure you want to remove $user from the $group : (y/n) ? " ans
            if [ "$ans" == "y" ]; then
                 sudo gpasswd -d "$user" "$group"
-                echo "$user has been removed from $group."
+                echo -e "\e[1;32m$user has been removed from $group.\e[0m"
 		return
            elif [ "$ans" == "n" ]; then
-                 echo "$user will not be removed from the group."
+                 echo -e "\e[1;32m$user will not be removed from the group.\e[0m"
            else
-                echo "Invalid option."
+                echo -e "\e[1;31Invalid option.\e[0m"
            fi
     else
-        echo "$group does not exist."
+        echo -e "\e[1;31m$group does not exist.\e[0m"
     fi
   else
-        echo "$user doesn't exist."
+        echo -e "\e[1;31mThe user '$user' doesn't exist.\e[0m"
   fi
 }
 
@@ -651,12 +652,12 @@ removeGroup(){
 #change a user's group membership
 groupSudo(){
  echo " "
- echo "================================  Change a user’s group membership ================================="
- echo "1) Add to a new group"
- echo "2) Remove from a group"
- echo "3) Back to user management"
- echo "4) Back to main menu"
- echo "5) Exit program"
+ echo -e "\e[1;97m================================  Change a user’s group membership =================================\e[0m"
+ echo -e "\e[1;38;5;214m1) Add to a new group\e[0m"
+ echo -e "\e[1;38;5;212m2) Remove from a group\e[0m"
+ echo -e "\e[38;5;51m3) Back to user management\e[0m"
+ echo -e "\e[1;37m4) Back to main menu\e[0m"
+ echo -e "\e[1;31m5) Exit program\e[0m"
 
  read -p "Select an option [1-5]: " opt
 
@@ -668,19 +669,19 @@ groupSudo(){
           removeGroup
         ;;
         3)
-          echo "Returning to user management"
+          echo -e "\e[38;5;51mReturning to user management\e[0m"
           UserManagement
 	;;
         4)
-         echo "Returning to Main Menu"
+         echo -e "\e[1;37mReturning to Main Menu...\e[0m"
           MainMenu
         ;;
 	5)
-	  echo "Ending program...."
+	  echo -e "\e[1;31mEnding program....\e[0m"
 	  exit 1
 	;;
         *)
-         echo "Invalid option."
+         echo -e "\e[1;31mInvalid option.\e[0m"
         ;;
   esac
 }
@@ -691,16 +692,16 @@ groupSudo(){
 UserManagement(){
 while true;do
   echo " "
-  echo -e "\e[1;30m=======================================  User Management =======================================\e[0m"
+  echo -e "\e[38;5;51m=======================================  User Management =======================================\e[0m"
   echo -e "\e[1;92m1) Add a user\e[0m"
-  echo "2) Grant root permission to a user"
-  echo "3) Delete a user"
-  echo "4) Display connected user"
-  echo "5) Disconnect a remote user"
-  echo "6) List all groups of a user"
-  echo "7) Change user's group membership"
-  echo "8) Return to Main Menu"
-  echo "9) Exit program"
+  echo -e "\e[1;91m2) Grant root permission to a user\e[0m"
+  echo -e "\e[1;94m3) Delete a user\e[0m"
+  echo -e "\e[1;95m4) Display connected user\e[0m"
+  echo -e "\e[1;96m5) Disconnect a remote user\e[0m"
+  echo -e "\e[1;93m6) List all groups of a user\e[0m"
+  echo -e "\e[1;97m7) Change user's group membership\e[0m"
+  echo -e "\e[1;90m8) Return to Main Menu\e[0m"
+  echo -e "\e[1;31m9) Exit program\e[0m"
   echo " "
 
   read -p "Select an option  [1-9]: " option
@@ -728,7 +729,7 @@ while true;do
           groupSudo
         ;;
 	8)
-	  echo "Returning to Main Menu"
+	  echo -e "\e[1;30mReturning to Main Menu\e[0m"
           MainMenu
 	;;
         9)
@@ -736,7 +737,7 @@ while true;do
          exit 1
         ;;
         *)
-          echo "Invalid option"
+          echo -e "\e[1;31mInvalid option\e[0m"
         ;;
   esac
 done
@@ -744,7 +745,7 @@ done
 
 #---------------------------------------File Management--------------------------------------
 userAndFileCheck(){
-  echo "======================  File check  ==========================="
+  echo -e "\e[38;5;141m======================  File Check  ===========================\e[0m"
    #asking user for username and check if the user exist
   read -p "Enter an username: " username
 
@@ -760,12 +761,12 @@ userAndFileCheck(){
 
     #check if its not empty
 	if [[ -n "$fileResult" ]]; then
-   	   echo "Path: $fileResult"
+   	   echo -e "\e[1;32mPath: $fileResult\e[0m"
 	else
-	   echo "File not found."
+	   echo -e "\e[1;31mFile not found.\e[0m"
 	fi
   else
-     echo "User does not exist."
+     echo -e "\e[1;31mUser does not exist.\e[0m"
      return 1
   fi
 }
@@ -776,10 +777,11 @@ display_large(){
   read -p "Enter a username: " username
   if id "$username" &>/dev/null; then
     User_Home=$(getent passwd "$username" | cut -d: -f6)
-    echo "==========================  10 Largest File  =========================="
-    sudo ls -lSh "$User_Home" | grep -v d | head -n 10 | awk '{print $9}'
+    echo -e "\e[38;5;226m========================  10 Largest File  ==========================\e[0m"
+    files=$(sudo ls -lSh "$User_Home" | grep -v d | head -n 10 | awk '{print $9}')
+    echo -e "\e[1;32m $files\e[0m"
   else
-    echo "User does not exist."
+    echo -e "\e[1;31mUser does not exist.\e[0m"
   fi
 }
 
@@ -790,11 +792,12 @@ display_old(){
   if id "$username" &>/dev/null; then
      #get the user's home directory
      User_Home=$(getent passwd "$username" | cut -d: -f6)
-    echo "==========================  10 Oldest File  =========================="
+    echo -e "\e[38;5;87m======================  10 Oldest File  ==========================\e[0m"
     #get the oldest file with ls -ltr. t sort by time and r, reverse the order so the oldest are on the top
-    sudo ls -ltr "$User_Home" | grep -v d | head -n 10 | awk '{print $9}'
+    files=$(sudo ls -ltr "$User_Home" | grep -v d | head -n 10 | awk '{print $9}')
+   echo -e "\e[1;32m $files\e[0m"
   else
-    echo "User does not exist."
+    echo -e "\e[1;31mUser does not exist.\e[0m"
   fi
 }
 
@@ -802,13 +805,12 @@ display_old(){
 FileManagement(){
 while true; do
   echo " "
-  echo "=======================================  File Management ======================================="
-  echo " "
-  echo "1) Check username and file existes"
-  echo "2) Display the 10 largest files in a user's home directorey"
-  echo "3) Diplay the 10 oldest files in a user's home directory"
-  echo "4) Back to Main Menu"
-  echo "5) End Program"
+  echo -e "\e[38;5;208m=======================================  File Management =======================================\e[0m"
+  echo -e "\e[38;5;141m1) Check username and file existes\e[0m"
+  echo -e "\e[38;5;226m2) Display the 10 largest files in a user's home directorey\e[0m"
+  echo -e "\e[38;5;87m3) Diplay the 10 oldest files in a user's home directory\e[0m"
+  echo -e "\e[1;37m4) Back to Main Menu\e[0m"
+  echo -e "\e[1;31m5) End Program\e[0m"
 
   read -p "Select an option [1-5]: " opt
 
@@ -823,15 +825,15 @@ while true; do
 	  display_old
 	;;
 	4)
-	  echo "Returning to Main Menu"
+	  echo -e "\e[1;37mReturning to Main Menu\e[0m"
 	  MainMenu
 	;;
 	5)
-	  echo "Ending program...."
+	  echo "\e[1;31mEnding program....\e[0m"
 	  exit 1
 	;;
 	*)
-	  echo "Invaild option"
+	  echo -e "\e[1;31mInvaild option\e[0m"
 	;;
   esac
 done
